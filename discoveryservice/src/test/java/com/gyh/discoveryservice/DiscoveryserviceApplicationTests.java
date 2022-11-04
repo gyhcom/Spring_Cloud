1b53726db5e0f963a12509afa53ed3f56ed532f0
gyhcom <gyeonho@gamil.com>
2022-11-04 23:40:16 +0900

    DiscoveryService

---
+++ discoveryservice/src/test/java/com/gyh/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyh.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
